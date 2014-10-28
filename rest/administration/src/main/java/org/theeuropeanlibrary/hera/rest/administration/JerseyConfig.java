7186909c8cd0a21b0c0401188aa5b65fc4fb0518
1nf1n1ty08 <drakash08>
2014-10-28 14:51:41 +0100

    moved object mapper and setup dynamic parts

--- rest/administration/src/main/java/org/theeuropeanlibrary/hera/rest/administration/JerseyConfig.java
+++ rest/administration/src/main/java/org/theeuropeanlibrary/hera/rest/administration/JerseyConfig.java
@@ -3,26 +3,26 @@ package org.theeuropeanlibrary.hera.rest.administration;
 import org.glassfish.jersey.filter.LoggingFilter;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.spring.scope.RequestContextFilter;
-import org.theeuropeanlibrary.hera.rest.administration.objectmapper.TelObjectMapper;
+import org.theeuropeanlibrary.maia.converter.json.EntityObjectMapper;
 
 /**
  * Jersey Configuration for Exception Mappers and Resources
- * 
+ *
  */
 public class JerseyConfig extends ResourceConfig {
 
-	public JerseyConfig() {
-		super();
-		
+    public JerseyConfig() {
+        super();
+
         register(ProviderResource.class);
         register(ProvidersResource.class);
-        
-        register(TelObjectMapper.class);
+
+        register(EntityObjectMapper.class);
 
         register(DatasetResource.class);
         register(DatasetsResource.class);
 
         register(RequestContextFilter.class);
         register(LoggingFilter.class);
-	}
+    }
 }
